6f53baf65fcb51991508c7c1daa20cb171ecb38d
Lakes2 <sakirajessica01@gmail.com>
2023-03-03 16:15:29 +0800

    feat: update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # 当发生错误时中止脚本
 set -e
 
-PROJECT="avedex-m-test-2-gh"
+PROJECT="avedex-m-test-10-gh"
 
 yarn
 # 构建
@@ -18,7 +18,7 @@ touch .nojekyll
 cd ..
 scp -r dist root@54.255.13.84:/root/code/$PROJECT-dist
 
-GITHUB_ACCESS_TOKEN='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'
+GITHUB_ACCESS_TOKEN='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'
 
 ssh root@54.255.13.84 << ENDSSH
 cd /root/code/;
@@ -28,7 +28,7 @@ cd $PROJECT;
 git init;
 git add -A;
 git commit -m 'deploy';
-git push -f https://$GITHUB_ACCESS_TOKEN@github.com/ave-m-test-2/ave-m-test-2.github.io.git master:main;
+git push -f https://$GITHUB_ACCESS_TOKEN@github.com/ave-m-test-10/ave-m-test-10.github.io.git master:main;
 
 ENDSSH
 
